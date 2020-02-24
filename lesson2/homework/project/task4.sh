0d6b0f4933df618e212e54cb6f4385a842714beb
Mikhail <mykhail.nakonechnyi@gmail.com>
2020-02-24 13:42:03 +0200

    hw2

---
+++ lesson2/homework/project/task4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir img
+touch img/image.txt
+mkdir css
+touch css/style.css
+mkdir js
+touch js/main.js
+touch index.html
---
+++ lesson2/homework/task3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir img
+mkdir css
+mkdir js
